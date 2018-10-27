acd321a50873b728a5dacb1b7d3aefeffa76eb02
damiendoumer <damientohin@gmail.com>
2018-10-27 18:21:42 +0200

    Finished operands

--- CPU/BoxOperand.cpp
+++ CPU/BoxOperand.cpp
@@ -13,25 +13,146 @@ eOperandType BoxOperand::getType() const {
     return type;
 }
 
-std::unique_ptr<IOperand> BoxOperand::operator-(const IOperand &rhs) const {
-    return std::unique_ptr<IOperand>();
+std::unique_ptr<IOperand> BoxOperand::operator-(const IOperand &rhs) const
+{
+    long double vrhs = 0;
+    long double vlhs = 0;
+    long double res = 0;
+
+    Converter::convertValue<long double>(eOperandType::BigDecimal, rhs.toString(), BigDecimalOperandType::_BigDecimal, vrhs);
+    Converter::convertValue<long double>(eOperandType::BigDecimal, value, BigDecimalOperandType::_BigDecimal, vlhs);
+    res = vlhs - vrhs;
+    string stringRes = std::to_string(res);
+    if(rhs.getType() == eOperandType::Int8 ||
+       rhs.getType() == eOperandType::Int16 ||
+       rhs.getType() ==  eOperandType::Int32)
+    {
+        auto result = Converter::conv(type, stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(result.value, type));
+    }
+    else if (rhs.getType() == eOperandType::Float)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::Double)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::BigDecimal)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
 }
 
-std::unique_ptr<IOperand> BoxOperand::operator*(const IOperand &rhs) const {
-    return std::unique_ptr<IOperand>();
+std::unique_ptr<IOperand> BoxOperand::operator*(const IOperand &rhs) const
+{
+    long double vrhs = 0;
+    long double vlhs = 0;
+    long double res = 0;
+
+    Converter::convertValue<long double>(eOperandType::BigDecimal, rhs.toString(), BigDecimalOperandType::_BigDecimal, vrhs);
+    Converter::convertValue<long double>(eOperandType::BigDecimal, value, BigDecimalOperandType::_BigDecimal, vlhs);
+    res = vlhs * vrhs;
+    string stringRes = std::to_string(res);
+    if(rhs.getType() == eOperandType::Int8 ||
+       rhs.getType() == eOperandType::Int16 ||
+       rhs.getType() ==  eOperandType::Int32)
+    {
+        auto result = Converter::conv(type, stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(result.value, type));
+    }
+    else if (rhs.getType() == eOperandType::Float)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::Double)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::BigDecimal)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
 }
 
-std::unique_ptr<IOperand> BoxOperand::operator/(const IOperand &rhs) const {
-    return std::unique_ptr<IOperand>();
+std::unique_ptr<IOperand> BoxOperand::operator/(const IOperand &rhs) const
+{
+    long double vrhs = 0;
+    long double vlhs = 0;
+    long double res = 0;
+
+    Converter::convertValue<long double>(eOperandType::BigDecimal, rhs.toString(), BigDecimalOperandType::_BigDecimal, vrhs);
+    Converter::convertValue<long double>(eOperandType::BigDecimal, value, BigDecimalOperandType::_BigDecimal, vlhs);
+    if(vlhs == 0)
+      throw AVMWarnException("Divide by 0 exception.");
+    res = vlhs / vrhs;
+    string stringRes = std::to_string(res);
+    if(rhs.getType() == eOperandType::Int8 ||
+       rhs.getType() == eOperandType::Int16 ||
+       rhs.getType() ==  eOperandType::Int32)
+    {
+        auto result = Converter::conv(type, stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(result.value, type));
+    }
+    else if (rhs.getType() == eOperandType::Float)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::Double)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::BigDecimal)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
 }
 
-std::unique_ptr<IOperand> BoxOperand::operator%(const IOperand &rhs) const {
-    return std::unique_ptr<IOperand>();
+std::unique_ptr<IOperand> BoxOperand::operator%(const IOperand &rhs) const
+{
+    long double vrhs = 0;
+    long double vlhs = 0;
+    long double res = 0;
+
+    Converter::convertValue<long double>(eOperandType::BigDecimal, rhs.toString(), BigDecimalOperandType::_BigDecimal, vrhs);
+    Converter::convertValue<long double>(eOperandType::BigDecimal, value, BigDecimalOperandType::_BigDecimal, vlhs);
+    res = fmod(vlhs, vrhs);
+    string stringRes = std::to_string(res);
+    if(rhs.getType() == eOperandType::Int8 ||
+       rhs.getType() == eOperandType::Int16 ||
+       rhs.getType() ==  eOperandType::Int32)
+    {
+        auto result = Converter::conv(type, stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(result.value, type));
+    }
+    else if (rhs.getType() == eOperandType::Float)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::Double)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
+    else if (rhs.getType() == eOperandType::BigDecimal)
+    {
+        auto result = Converter::conv(rhs.getType(), stringRes);
+        return std::unique_ptr<IOperand>(new BoxOperand(stringRes, rhs.getType()));
+    }
 }
 
 std::unique_ptr<IOperand> BoxOperand::operator+(const IOperand &rhs) const
 {
-
     long double vrhs = 0;
     long double vlhs = 0;
     long double res = 0;
--- CPU/Controller.cpp
+++ CPU/Controller.cpp
@@ -178,7 +178,7 @@ void Controller::add()
     eOperandType t = result.get()->getType();
     BoxOperand *myOp = new BoxOperand(s, t);
     MyStack::getInstance()->push(std::unique_ptr<BoxOperand>(myOp));
-    cout << " Finished subtracting values, result yields: " << s << "\n";
+    cout << "Finished adding values, result yields: " << s << "\n";
 }
 
 void Controller::swap()
@@ -189,24 +189,50 @@ void Controller::swap()
 
 void Controller::sub()
 {
-    int *v = 0;
-    Converter::convertValue<int>(eOperandType::Int8, "20", IntOperandType::_Int8, *v);
-    int i = *v;
+    BoxOperand *poped = MyStack::getInstance()->pop();
+    BoxOperand *poped2 = MyStack::getInstance()->pop();
+    std::unique_ptr<IOperand> result = poped2->operator-(*poped);
+    string s = result.get()->toString();
+    eOperandType t = result.get()->getType();
+    BoxOperand *myOp = new BoxOperand(s, t);
+    MyStack::getInstance()->push(std::unique_ptr<BoxOperand>(myOp));
+    cout << " Finished subtracting values, result yields: " << s << "\n";
 }
 
 void Controller::mul()
 {
-
+    BoxOperand *poped = MyStack::getInstance()->pop();
+    BoxOperand *poped2 = MyStack::getInstance()->pop();
+    std::unique_ptr<IOperand> result = poped2->operator*(*poped);
+    string s = result.get()->toString();
+    eOperandType t = result.get()->getType();
+    BoxOperand *myOp = new BoxOperand(s, t);
+    MyStack::getInstance()->push(std::unique_ptr<BoxOperand>(myOp));
+    cout << " Finished subtracting values, result yields: " << s << "\n";
 }
 
 void Controller::div()
 {
-
+    BoxOperand *poped = MyStack::getInstance()->pop();
+    BoxOperand *poped2 = MyStack::getInstance()->pop();
+    std::unique_ptr<IOperand> result = poped2->operator/(*poped);
+    string s = result.get()->toString();
+    eOperandType t = result.get()->getType();
+    BoxOperand *myOp = new BoxOperand(s, t);
+    MyStack::getInstance()->push(std::unique_ptr<BoxOperand>(myOp));
+    cout << " Finished Dividing values, result yields: " << s << "\n";
 }
 
 void Controller::mod()
 {
-
+    BoxOperand *poped = MyStack::getInstance()->pop();
+    BoxOperand *poped2 = MyStack::getInstance()->pop();
+    std::unique_ptr<IOperand> result = poped2->operator%(*poped);
+    string s = result.get()->toString();
+    eOperandType t = result.get()->getType();
+    BoxOperand *myOp = new BoxOperand(s, t);
+    MyStack::getInstance()->push(std::unique_ptr<BoxOperand>(myOp));
+    cout << " Finished Modulus, result yields: " << s << "\n";
 }
 
 void Controller::load()
